7dfedbdb3a8690f85db54530c4b7f753b1157cce
KiranBai <157038014+KiranBai@users.noreply.github.com>
2024-08-04 22:01:06 +0500

    Update ProductCard.jsx
    Plantry Tracker

--- Plantry_Tracker/src/components/productCard/ProductCard.jsx
+++ Plantry_Tracker/src/components/productCard/ProductCard.jsx
@@ -16,11 +16,19 @@ function ProductCard() {
                     <div className="p-4 md:w-1/4  drop-shadow-lg " >
                         <div className="h-full border-2 hover:shadow-gray-100 hover:shadow-2xl transition-shadow duration-300 ease-in-out    border-gray-200 border-opacity-60 rounded-2xl overflow-hidden" style={{ backgroundColor: mode === 'dark' ? 'rgb(46 49 55)' : '', color: mode === 'dark' ? 'white' : '', }} >
                             <div className="flex justify-center cursor-pointer" >
-                                <img className=" rounded-2xl w-full h-80 p-2 hover:scale-110 transition-scale-110  duration-300 ease-in-out" src="https://dummyimage.com/720x400" alt="blog" />
+                                <img className=" rounded-2xl w-full h-80 p-2 hover:scale-110 transition-scale-110  duration-300 ease-in-out" src="https://www.knuckleheadsclothing.com/cdn/shop/products/kids-long-sleeve-shirt_18a4214c-b5f8-4288-80ab-70765a3d0ba4_1000x.png?v=1660846672" alt="blog" />
                             </div>
                             <div className="p-5 border-t-2">
                                 <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1" style={{ color: mode === 'dark' ? 'white' : '', }}>Kiran's Plantry Tracker</h2>
-                                <h1 className="title-font text-lg font-medium text-gray-900 mb-3" style={{ color: mode === 'dark' ? 'white' : '', }}>This is title</h1>
+                                <h1 className="title-font text-lg font-medium text-gray-900 mb-3" style={{ color: mode === 'dark' ? 'white' : '', }}>Burgundy & Blue Plaid Button Down Long Sleeve Shirt For Children. 
+
+
+Button-front shirt with long sleeves
+Designed in the USA / Imported
+Machine Wash
+Pearl snap buttons
+100 % Money Back Guarantee
+100% Cotton</h1>
                                 {/* <p className="leading-relaxed mb-3">{item.description.}</p> */}
                                 <p className="leading-relaxed mb-3" style={{ color: mode === 'dark' ? 'white' : '' }}>₹ 500</p>
                                 <div className=" flex justify-center">
@@ -35,11 +43,11 @@ function ProductCard() {
                     <div className="p-4 md:w-1/4  drop-shadow-lg " >
                         <div className="h-full border-2 hover:shadow-gray-100 hover:shadow-2xl transition-shadow duration-300 ease-in-out    border-gray-200 border-opacity-60 rounded-2xl overflow-hidden" style={{ backgroundColor: mode === 'dark' ? 'rgb(46 49 55)' : '', color: mode === 'dark' ? 'white' : '', }} >
                             <div className="flex justify-center cursor-pointer" >
-                                <img className=" rounded-2xl w-full h-80 p-2 hover:scale-110 transition-scale-110  duration-300 ease-in-out" src="https://dummyimage.com/720x400" alt="blog" />
+                                <img className=" rounded-2xl w-full h-80 p-2 hover:scale-110 transition-scale-110  duration-300 ease-in-out" src="https://m.media-amazon.com/images/I/71WkqhyzfJL._AC_SX679_.jpg" alt="blog" />
                             </div>
                             <div className="p-5 border-t-2">
                                 <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1" style={{ color: mode === 'dark' ? 'white' : '', }}>Kiran's Plantry Tracker</h2>
-                                <h1 className="title-font text-lg font-medium text-gray-900 mb-3" style={{ color: mode === 'dark' ? 'white' : '', }}>This is title</h1>
+                                <h1 className="title-font text-lg font-medium text-gray-900 mb-3" style={{ color: mode === 'dark' ? 'white' : '', }}>Mens Shirts Long Sleeve - Linen Shirts Cool Breathable Print Shirt Casual Lapel Shirt Long Sleeve Personalised Buttons Placket Shirt Tops Funky Sports Shirts Long Sleeve for Everyday Wear</h1>
                                 {/* <p className="leading-relaxed mb-3">{item.description.}</p> */}
                                 <p className="leading-relaxed mb-3" style={{ color: mode === 'dark' ? 'white' : '' }}>₹ 500</p>
                                 <div className=" flex justify-center">
@@ -54,11 +62,11 @@ function ProductCard() {
                     <div className="p-4 md:w-1/4  drop-shadow-lg " >
                         <div className="h-full border-2 hover:shadow-gray-100 hover:shadow-2xl transition-shadow duration-300 ease-in-out    border-gray-200 border-opacity-60 rounded-2xl overflow-hidden" style={{ backgroundColor: mode === 'dark' ? 'rgb(46 49 55)' : '', color: mode === 'dark' ? 'white' : '', }} >
                             <div className="flex justify-center cursor-pointer" >
-                                <img className=" rounded-2xl w-full h-80 p-2 hover:scale-110 transition-scale-110  duration-300 ease-in-out" src="https://dummyimage.com/720x400" alt="blog" />
+                                <img className=" rounded-2xl w-full h-80 p-2 hover:scale-110 transition-scale-110  duration-300 ease-in-out" src="https://img.buzzfeed.com/buzzfeed-static/static/2019-11/13/19/asset/54a200614253/sub-buzz-1627-1573672920-1.jpg?downsize=900:*&output-format=auto&output-quality=auto" alt="blog" />
                             </div>
                             <div className="p-5 border-t-2">
                                 <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1" style={{ color: mode === 'dark' ? 'white' : '', }}>Kiran's Plantry Tracker</h2>
-                                <h1 className="title-font text-lg font-medium text-gray-900 mb-3" style={{ color: mode === 'dark' ? 'white' : '', }}>This is title</h1>
+                                <h1 className="title-font text-lg font-medium text-gray-900 mb-3" style={{ color: mode === 'dark' ? 'white' : '', }}>Men's Classic Leather Jacket. Crafted from high-quality, genuine leather, this jacket is designed to offer both durability and a sleek, modern look. The soft lining ensures comfort, while the sturdy zippers and multiple pockets provide functionality.</h1>
                                 {/* <p className="leading-relaxed mb-3">{item.description.}</p> */}
                                 <p className="leading-relaxed mb-3" style={{ color: mode === 'dark' ? 'white' : '' }}>₹ 500</p>
                                 <div className=" flex justify-center">
@@ -73,11 +81,11 @@ function ProductCard() {
                     <div className="p-4 md:w-1/4  drop-shadow-lg " >
                         <div className="h-full border-2 hover:shadow-gray-100 hover:shadow-2xl transition-shadow duration-300 ease-in-out    border-gray-200 border-opacity-60 rounded-2xl overflow-hidden" style={{ backgroundColor: mode === 'dark' ? 'rgb(46 49 55)' : '', color: mode === 'dark' ? 'white' : '', }} >
                             <div className="flex justify-center cursor-pointer" >
-                                <img className=" rounded-2xl w-full h-80 p-2 hover:scale-110 transition-scale-110  duration-300 ease-in-out" src="https://dummyimage.com/720x400" alt="blog" />
+                                <img className=" rounded-2xl w-full h-80 p-2 hover:scale-110 transition-scale-110  duration-300 ease-in-out" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpxdW2yMu23GnItiKF4E2I25boiU-D6g9Gkg&s" alt="blog" />
                             </div>
                             <div className="p-5 border-t-2">
                                 <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1" style={{ color: mode === 'dark' ? 'white' : '', }}>Kiran's Plantry Tracker</h2>
-                                <h1 className="title-font text-lg font-medium text-gray-900 mb-3" style={{ color: mode === 'dark' ? 'white' : '', }}>This is title</h1>
+                                <h1 className="title-font text-lg font-medium text-gray-900 mb-3" style={{ color: mode === 'dark' ? 'white' : '', }}>esigned to offer both comfort and style, these jeans are made from a high-quality denim blend that provides flexibility and durability. The slim-fit cut ensures a modern silhouette, making these jeans perfect for both casual and semi-formal occasionsesigned to offer both comfort and style, these jeans are made from a high-quality denim blend that provides flexibility and durability. The slim-fit cut ensures a modern silhouette, making these jeans perfect for both casual and semi-formal occasions</h1>
                                 {/* <p className="leading-relaxed mb-3">{item.description.}</p> */}
                                 <p className="leading-relaxed mb-3" style={{ color: mode === 'dark' ? 'white' : '' }}>₹ 500</p>
                                 <div className=" flex justify-center">
